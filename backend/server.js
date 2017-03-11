c1af1f6436097fba99e3969b28a5c05263975ade
John Zhou <john.km.zhou@gmail.com>
2017-03-11 12:40:30 -1000

    Fixed input, added test for analog or digital

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ const socket = require('express-ws')(app);
 
 // setup pi connection
 gpio.wiringPiSetup();
-gpio.pinMode(8, gpio.modes.INPUT);
+gpio.pinMode(8, gpio.INPUT);
 
 const tilt = () => {
-	return gpio.analogRead(8);
+	// testing whether it's digital or analog
+	// return gpio.analogRead(8);
+	return gpio.digitalRead(8);
 }
 
 //app.use(express.static('./public'));
@@ -34,11 +36,11 @@ app.ws('/', function(ws, req) {
 
 });
 
-	// testing
-	setInterval(()=>{
-		let tilt_value = tilt();
-		console.log(tilt_value)
-	}, 1000);
+// testing
+setInterval(()=>{
+	let tilt_value = tilt();
+	console.log(tilt_value)
+}, 500);
 
 app.listen(3000, () => {
 	console.info('Server started on port 3000');

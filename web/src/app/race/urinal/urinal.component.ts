477860899f98c521aed8cc4646f52263ae02a3ef
Felipe Martina <felipe.martina@3xmgroup.com>
2017-03-11 18:09:33 -1000

    Slowed down the increments

--- web/src/app/race/urinal/urinal.component.ts
+++ web/src/app/race/urinal/urinal.component.ts
@@ -8,7 +8,7 @@ import { Urinal } from '../urinal'
 export class UrinalComponent implements OnInit {
   @Input() urinal: Urinal;
   maskUrinalinitialPosition = 282.36221
-  constantPixelIncrement = 10
+  constantPixelIncrement = 5
   maskUrinalDAttribute = `m 129.9,282.36221 c -4.71405,0 -10,5.28595 -10,10 l 0,240 c 0,24.26703 45.96753,25.63413 70,29 16.50557,2.31169 33.49443,2.31169 50,0 24.03247,-3.36587 70,-4.73297 70,-29 l 0,-240 c 0,-4.71405 -5.28595,-10 -10,-10 z`;
   horseImage = '../../../assets/images/horse.gif'
   constructor(private elementRef: ElementRef) { }
